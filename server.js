e6ed5827d912f6a3bf3d092a597d525152e889ce
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-05-10 13:07:41 -0400

    Added read file functionality

--- server.js
+++ server.js
@@ -7,23 +7,30 @@ http.createServer(function(request, response) {
   let q = url.parse(addr, true);
   let filePath = "";
 
-  fs.appendFile("log.txt", `URL: ${filePath}\n Timestamp: ${new Date()}\n\n`, 
+  fs.appendFile("log.txt", "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n", 
   function(err) {
     if(err) {
       console.log(err);
     }
     else {
-      console.lof("Added to log");
+      console.log("Added to log");
     }
   })
 
   if(q.pathname.includes("documentation")) {
-    filePath = __dirname + "documentation.html";
+    filePath = (__dirname + "/documentation.html");
   }
   else {
-    filePath = "index.html"
+    filePath = "index.html";
   }
 
-  response.writeHead(200, {"Content Type": "text/plain"});
-  response.end("Hello Node!\n");
+  fs.readFile(filePath, function(err, data) {
+    if(err) {
+      throw err;
+    }
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(data);
+    response.end();
+  });
 }).listen(8080);
+console.log("My server is running on port 8080");
