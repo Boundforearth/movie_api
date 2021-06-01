df7232af3e28c825377dd9da6e197d6b1c86c7ca
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-06-01 12:43:25 -0400

    added jwt authentication

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const Models = require("./models.js");
 const app = express();
 app.use(bodyParser.json());
 
+let auth = require('./auth')(app);
+
+const passport = require('passport');
+require('./passport');
+
 const Movies = Models.Movie;
 const Users = Models.User;
 //Morgan will log basic info to console
@@ -25,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 //get list of movies
-app.get("/movies", (req, res) => {
+app.get("/movies", passport.authenticate("jwt", {session: false}),(req, res) => {
   Movies.find().then((movies) => {
     res.status(201).json(movies);
   }).catch((err) => {
@@ -40,7 +45,7 @@ app.get("/", (req, res) => {
 });
 
 //Get information on a particular movie
-app.get("/movies/:movie", (req, res) => {
+app.get("/movies/:movie", passport.authenticate("jwt", {session: false}), (req, res) => {
   Movies.findOne({Title: req.params.movie})
     .then((movies) => {
       res.status(201).json(movies);
@@ -77,7 +82,10 @@ app.post("/users", (req, res) => {
     });
 });
 
-app.get("/users/:user", (req, res) => {
+
+//Get a users information.  Useful for testing and checking if information updated or deleted
+//Remove before final release as it displays passwords, birthdays, and emails.
+app.get("/users/:user", passport.authenticate("jwt", {session: false}), (req, res) => {
   Users.findOne({Username: req.params.user})
     .then((user) => {
       res.status(201).json(user);
@@ -88,25 +96,37 @@ app.get("/users/:user", (req, res) => {
 });
 
 //Allows a username to update their information
-app.put("/users/:username", (req, res) => {
-  Users.findOneAndUpdate({Username: req.params.username}, 
-    {$set: {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    }})
+app.put("/users/:username", passport.authenticate("jwt", {session: false}), (req, res) => {
+  //Checks if the new username exists first
+  Users.findOne({Username: req.body.Username})
     .then((user) => {
-      res.send("Your profile information has been updated!");
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    })
-});
+      if(user) {
+        //If the username already exists, the change is rejected
+        res.status(400).send("That username is already taken, please choose a different one.");
+      }
+      else {
+        //If the new username does not exist, this code runs to update to the new username.
+        Users.findOneAndUpdate({Username: req.params.username},
+          {$set: {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          }},
+          {new: true})
+          .then((user) => {
+            res.status(201).send("Your username has been updated to " + user.Username);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send("Error: " + err);
+          })
+      }
+   });
+  });
 
 //Allows a user to view their list of favorite movies
-app.get("/users/:username/mylist", (req, res) => {
+app.get("/users/:username/mylist", passport.authenticate("jwt", {session: false}), (req, res) => {
   Users.findOne({Username: req.params.username})
     .then((user) => {
       res.status(201).json(user.Favorites);
@@ -118,13 +138,14 @@ app.get("/users/:username/mylist", (req, res) => {
 });
 
 //Adds a movie to a users list of favorite movies by using the movieid as a parameter
-app.post("/users/:username/mylist/:movieid", (req, res) => {
+app.post("/users/:username/mylist/:movieid", passport.authenticate("jwt", {session: false}), (req, res) => {
   Users.findOne({Username: req.params.username})
     .then((user) => {
       let favoritesArray = user.Favorites;
       console.log(favoritesArray);
       let sameMovieExists;
       let myMovie = req.params.movieid;
+      //loop to run through array and check whether or not a movie is in the user's Favorites
       for(i = 0; i < favoritesArray.length; i++) {
         if(favoritesArray[i] == myMovie) {
           sameMovieExists = myMovie;
@@ -134,6 +155,7 @@ app.post("/users/:username/mylist/:movieid", (req, res) => {
       if(sameMovieExists === req.params.movieid) {
         res.status(400).send("That movie is already in your favorites!")
       }
+      //if movie not in favorites, this runs to update the code
       else {
         Users.findOneAndUpdate({Username: req.params.username},
           {$push: { Favorites: req.params.movieid}})
@@ -152,7 +174,7 @@ app.post("/users/:username/mylist/:movieid", (req, res) => {
 
 
 //Deletes a movie from a users favorites movie list by using the id as a parameter.
-app.delete("/users/:username/mylist/:movie", (req, res) => {
+app.delete("/users/:username/mylist/:movie", passport.authenticate("jwt", {session: false}), (req, res) => {
   Users.findOneAndUpdate({Username: req.params.username}, 
     {$pull: {Favorites: req.params.movie}})
     .then((user) => {
@@ -164,7 +186,8 @@ app.delete("/users/:username/mylist/:movie", (req, res) => {
     });
 });
 
-app.delete("/users/:username", (req, res) => {
+//Deletes a user from the database
+app.delete("/users/:username", passport.authenticate("jwt", {session: false}), (req, res) => {
   let username = req.params.username;
   Users.findOneAndRemove({Username: req.params.username})
     .then((user) => {
@@ -182,7 +205,7 @@ app.delete("/users/:username", (req, res) => {
 });;
 
 //Looks in the Movies collection for a document containing the desired genre, then displays that genre's description
-app.get("/genres/:genre", (req, res) => {
+app.get("/genres/:genre", passport.authenticate("jwt", {session: false}), (req, res) => {
   Movies.findOne({"Genre.Name": req.params.genre})
     .then((genre) => {
       res.json(genre.Genre);
@@ -193,7 +216,7 @@ app.get("/genres/:genre", (req, res) => {
 });
 
 //Looks in Movies collection for a document containing the desired director, then displays that director's information
-app.get("/directors/:director", (req, res) => {
+app.get("/directors/:director", passport.authenticate("jwt", {session: false}), (req, res) => {
   Movies.findOne({"Director.Name": req.params.director})
     .then((director) => {
       res.json(director.Director);
