def53d7684ba064d6b815d16d7fe41696f596ce4
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-09-09 20:47:59 -0400

    Possible error fix with Angular

--- index.js
+++ index.js
@@ -263,10 +263,10 @@ app.delete("/users/:username", passport.authenticate("jwt", {session: false}), (
   Users.findOneAndRemove({Username: req.params.username})
     .then((user) => {
       if(!user) {
-        res.status(400).send(username+ " was not found.");
+        res.status(400).send("Username not found");
       }
       else {
-        res.status(200).send(username + " was deleted.");
+        res.status(200).send("Username Deleted");
       }
     })
     .catch((err) => {
